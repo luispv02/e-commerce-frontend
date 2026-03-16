c2da409220b5cebc2e921e2155ed3d4ed1d2ac37
Luis Fernando <luispv.1002@gmail.com>
2026-03-15 23:56:04 -0600

    test: setup jest configuration

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  transform: { "^.+\\.tsx?$": "ts-jest", },
+  moduleNameMapper: { "\\.(css|less|sass|scss)$": "identity-obj-proxy" },
+};
+
+
+export default config;
